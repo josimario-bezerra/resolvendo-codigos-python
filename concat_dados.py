369ff86b0099c3fd8e929350b03b87d02e8c97af
josimario-bezerra <josimarior@outlook.com>
2025-08-12 16:51:16 +0000

    Criando código

---
+++ concat_dados.py
@@ -0,0 +1,10 @@
+#Receber dois dados diferentes do usuário e concatena-los em uma única string
+
+dado1 = input("Digite a primeira informação: ")
+dado2 = input("Digite a segunda informação: ")
+
+# Concatenar os dois valores em uma única string
+resultado = dado1 + ' ' + dado2
+
+# Exibir o resultado
+print(resultado)
